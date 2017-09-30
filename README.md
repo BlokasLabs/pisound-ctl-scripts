e40379a9283fe7f97ae3f2a72ac2f34d1cded2f6
Giedrius <giedrius@blokas.io>
2017-09-30 12:58:53 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pisound-ctl-scripts
+
+The repository for Pisound App scripts. See https://blokas.io/pisound/docs/Pisound-App for more details.
---
+++ patch_list.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Copyright (C) 2017 Vilniaus Blokas UAB, https://blokas.io/pisound
+# All rights reserved.
+#
+# This software may be modified and distributed under the terms
+# of the BSD license.  See the LICENSE file for details.
+#
+
+find /usr/local/etc/pisound-patches/ -type f -name "main.pd"
+find /usr/local/etc/puredata-patches/ -type f -name "main.pd"
