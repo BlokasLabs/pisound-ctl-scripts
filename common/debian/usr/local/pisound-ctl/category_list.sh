16844e7c174ed83b8cae3f3299e6bb233d85c1ef
Giedrius <giedrius@blokas.io>
2020-03-07 16:26:35 +0000

    Dynamic category listing

--- common/debian/usr/local/pisound-ctl/category_list.sh
+++ common/debian/usr/local/pisound-ctl/category_list.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# Copyright (C) 2017-2018 Vilniaus Blokas UAB, https://blokas.io/pisound
+# Copyright (C) 2017-2020 Vilniaus Blokas UAB, https://blokas.io/pisound
 # All rights reserved.
 #
 # This software may be modified and distributed under the terms
 # of the BSD license.  See the LICENSE file for details.
 #
 
-echo system/system.yml
-echo puredata/puredata.yml
+CURDIR=$(dirname $(readlink -f $0))
+find "${CURDIR}" -maxdepth 2 -mindepth 2 -name "*.yml"
